1112363a33f635df6eceae35838c2f5d01a37994
Ilian Sharkov <62841691+Li4kataaa@users.noreply.github.com>
2023-05-31 17:35:34 +0300

    Add files via upload

---
+++ 06. C# DB Advanced - Entity Framework Core/Final Exam/Exam/VaporStore/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace VaporStore.Data
+{
+	public static class Configuration
+	{
+		public static string ConnectionString =
+			@"Server=DESKTOP-GHV5K6M\MSSQLSERVER01;Database=VaporStore;Trusted_Connection=True";
+	}
+}
